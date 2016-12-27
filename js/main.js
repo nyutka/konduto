c5d2d3cf6e6febd521055ff6e7f16911dbc7b4f4
Anne <anne.tsugunyan@gmail.com>
2016-12-27 04:54:47 +0400

    Homepage, planos page

--- js/main.js
+++ js/main.js
@@ -1,42 +1,9 @@
-$(function() {
-
-	// Navbar close on click //
-	$(document).click(function (event) {
-		var clickover = $(event.target);
-		var $navbar = $(".navbar-collapse");               
-		var _opened = $navbar.hasClass("in");
-		if (_opened === true && !clickover.hasClass("navbar-toggle")) {      
-			$navbar.collapse('hide');
+ $(function(){
+	$('.selectpicker').on('change', function () {
+		var url = $(this).val();
+		if (url) {
+			window.location = url;
 		}
+		return false;
 	});
-
-	// Slider //
-	$('.our-clients-slider').slick({
-		infinite: false,
-		slidesToShow: 7,
-		responsive: [
-			{
-				breakpoint: 1200,
-				settings: {
-					infinite: false,
-					slidesToShow: 6
-				}
-			},
-			{
-				breakpoint: 992,
-				settings: {
-					infinite: false,
-					slidesToShow: 5
-				}
-			},
-			{
-			breakpoint: 768,
-				settings: {
-					infinite: false,
-					slidesToShow: 3
-				}
-			}
-		]
-	});
-
 });
